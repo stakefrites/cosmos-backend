94faf9e3b26d12af714e7875f37fbdb8a3c88c0f
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-12 23:28:26 -0400

    add routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 import { AccountHandler } from './utils/Wallet';
-import { DatabaseHandler } from "./db/controller";
+import { DatabaseHandler } from './db/controller';
 
 //import cron from "node-cron";
 import { IAccountConfig } from './types/Wallet';
 
+import AuthRouter from "./routes/auth";
 
 const myAccounts: IAccountConfig[] = [
     {
@@ -19,18 +20,46 @@ const myAccounts: IAccountConfig[] = [
         'evmosAddress': "evmos1apkrevnfre3k4eds8j7m79cmvrl6c5jn9en55j"
     }
 ]
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000;
+const db = new DatabaseHandler();
+
+app.use(express.json())
+app.use("/auth", AuthRouter);
+
 
 app.get("/", async (req, res) => {
     const portfolio = await AccountHandler.Create(
         myAccounts,
-        ["cosmoshub", "juno", "akash", "sifchain", "evmos"]
+        ["cosmoshub", "juno", "akash", "sifchain", "evmos"],
+        "62a6a05db00bfd49ba4e9681"
     );
-    const all = portfolio;
-    res.json(all);
+    const all = portfolio.serialize();
+    const created = await db.createAccount(all);
+    res.json({
+        status: "success",
+        account: created._id
+    });
+
 })
+
+app.get("/account/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const found = await db.getAccount(userId);
+    if (!found) { 
+        res.json({
+            status: "error",
+            message: "Not found!"
+        })
+    } else {
+        res.json({
+            status: "success",
+            account: found
+        });
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port} `);
