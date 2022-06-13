1ee09a9c7b795743d17a15ad325f72085bdb35d0
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-12 23:27:22 -0400

    update controller

--- src/db/controller.ts
+++ src/db/controller.ts
@@ -1,22 +1,38 @@
 import mongoose from 'mongoose';
-import { IAddAccount } from '../types/Wallet';
-import { AccountModel } from './models';
+import { IAddAccount, IUser, IAccount } from '../types/Wallet';
+import { AccountModel, UserModel } from './models';
+import * as dotenv from 'dotenv';
 
-mongoose.connect(process.env.MONGO_DB_URI || 'mongodb://localhost/test');
+dotenv.config();
+mongoose.connect(process.env.MONGO_DB_URI || 'mongodb+srv://localhost:27017');
 
 
 
+export class DatabaseHandler {
+    createUser = async (u: IUser) => {
+        const user = await UserModel.create({
+            username: u.username,
+            password: u.password
+        });
+        return user;
+    }
 
-export class DatabaseHandler { 
-    addAccount = async (config: IAddAccount ) => { 
-    const account = AccountModel.create({
-        userId: config.userId,
-        addresses: config.addresses,
-        portfolios: config.portfolios
-    })
-
-    console.log(account);
-    return account;
-
-}    
+    getUserByUsername = async (username: string) => { 
+        const user = await UserModel.findOne({ username });
+        if (!user) { 
+            return false
+        }
+        return user;
+    }
+    createAccount = async (a: IAccount) => {
+        const account = await AccountModel.create(a);
+        return account
+    }
+    getAccount = async (userId: string) => { 
+        const account = await AccountModel.findOne({ userId });
+        if (!account) { 
+            return false
+        }
+        return account
+    }
 }
