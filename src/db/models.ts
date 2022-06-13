4a2b35427d516cda4f918ff63a6de84b530cd238
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-12 23:26:48 -0400

    update models

--- src/db/models.ts
+++ src/db/models.ts
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
-import { IAccount } from "../types/Database";
+import { IAccount, IUser } from "../types/Wallet";
 
 
 // @TODO: Update schema after adding the helper function that gets the normalized data
 const accountSchema = new mongoose.Schema({
-  userId: {type: String, required: true},
-  addresses: [String],
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", unique: true },
+  tokens: mongoose.Schema.Types.Mixed,
+  portfolios: mongoose.Schema.Types.Mixed,
+  accounts: mongoose.Schema.Types.Mixed
 });
+
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, require: true, unique: true },
+  password: {type: String, required: true}
+})
+
+export const UserModel = mongoose.model<IUser>("User", userSchema)
 
 export const AccountModel = mongoose.model<IAccount>("Account", accountSchema);
