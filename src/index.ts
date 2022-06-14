fb6e41eeee40450dabbaf40e4e4da3b886162126
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-13 23:14:56 -0400

    cors + tokens router

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 import TrakmosRouter from "./routes/trakmos";
-import {refreshTrakmosAccountsJob } from "./jobs/job"
+import TokensRouter from "./routes/tokens";
+import { refreshTrakmosAccountsJob, refreshPricesJob } from './jobs/job';
 
 
 const port = process.env.PORT || 3000;
@@ -9,11 +11,14 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 refreshTrakmosAccountsJob.start();
+refreshPricesJob.start();
 
 
 
 app.use(express.json())
+app.use(cors())
 app.use("/trakmos", TrakmosRouter);
+app.use("/tokens", TokensRouter);
 
 
 app.get("/", async (req, res) => {
