3af74bb2a5ad4cdfc652a017a3a51ad896958eb9
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-07-03 15:43:15 -0400

    dev port

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import {
 import TokensRouter from './routes/tokens';
 import TrakmosRouter from './routes/trakmos';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
